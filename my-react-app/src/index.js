713aa762883b7016b9e5ded9ea64c02158c9c609
Leonardo-Costa-9000 <randeste@outlook.com>
2023-07-04 11:23:19 -0400

    removed unused files

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,27 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-//import Clock from "./clock";
-//import ControlledInputForm from "./controlledInputForms";
 import RandomNameGenerator from "./RandomNameGenerator";
 import "./index.css"; 
 
 
 function MyMainComponent() {
-  const [count, setCount] = useState(0);
 
-  const decreaseCount = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    }
-  };
 
-  /*const link = () => {
-    return (
-      <a href="https://www.nba.com">
-        <h1>Click me!</h1>
-      </a>
-    );
-  };*/
 
   return (
     <div>
